29faa0f4e580303ff9baeb1c67c53efeda7b4668
small_sunshine <jioo0224@naver.com>
2017-08-13 17:01:05 +0900

    테스트

--- src/module_command/command_help.js
+++ src/module_command/command_help.js
@@ -4,11 +4,13 @@ module.exports = (bot, logger, modules) => {
     bot.onText(new RegExp('^\/(?:help|도움말)+(?:@'+global.botinfo.username+')? ?$'), async (msg, match) => {
         if(Math.round((new Date()).getTime() / 1000) - msg.date <= 180) {
             const chatid = msg.chat.id;
-			let temp;
+			let temp, chatAction;
 			try {
 				logger.info('chatid: '+chatid+', username: '+modules.getuser(msg.from)+', lang: '+msg.from.language_code+', command: '+msg.text+', type: command received');
-				temp = await modules.getlang(msg, logger);
-				bot.sendChatAction(chatid, 'typing');
+				[temp, chatAction] = await Promise.all([
+					modules.getlang(msg, logger),
+					bot.sendChatAction(chatid, 'typing')
+				]);
 				await bot.sendMessage(chatid, "📒 "+temp.help('command.help.help'), {reply_to_message_id: msg.message_id, parse_mode: 'HTML', reply_markup: {
 					inline_keyboard: modules.commandlist(temp)
 				}});
