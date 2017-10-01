9e9260b1244a5cefe47e1504dd7dae647c3920c6
small_sunshine <jioo0224@naver.com>
2017-10-02 00:23:34 +0900

    오타 수정

--- src/module_message/google.js
+++ src/module_message/google.js
@@ -3,9 +3,9 @@
 module.exports = async (bot, logger, modules, msg) => {
     try {
         let temp = await modules.getlang(msg, logger);
-        if(msg.reply_to_message.text.match(new Regexp("🔍 "+temp.text(msg.chat.type, 'command.search.blank'))) != null ) {
+        if(msg.reply_to_message.text.match(new RegExp("🔍 "+temp.text(msg.chat.type, 'command.search.blank'))) != null ) {
             require('./google_search.js')(bot, logger, modules, msg, temp); //google search
-        } else if(msg.reply_to_message.text.match(new Regexp("🖼 "+temp.text(msg.chat.type, 'command.img.blank'))) != null) {
+        } else if(msg.reply_to_message.text.match(new RegExp("🖼 "+temp.text(msg.chat.type, 'command.img.blank'))) != null) {
             require('./google_image.js')(bot, logger, modules, msg, temp); //img search
         }
     } catch(e) {
