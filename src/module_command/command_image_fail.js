a30377c09d6a068bedefad835ca394c4b693b447
small_sunshine <jioo0224@naver.com>
2017-11-14 20:28:54 +0900

    img, lang 명령어 수정

--- src/module_command/command_image_fail.js
+++ src/module_command/command_image_fail.js
@@ -1,28 +1,5 @@
 "use strict";
 
-module.exports = (bot, logger, modules) => {
-    bot.onText(new RegExp('^\/(?:짤|이미지|사진|img|image|pic)+(?:@'+global.botinfo.username+')? ?$'), async(msg, match) => {
-		if(Math.round((new Date()).getTime() / 1000) - msg.date <= 180) {
-			const chatid = msg.chat.id;
-			let temp;
-			try {
-				logger.info('chatid: '+chatid+', username: '+modules.getuser(msg.from)+', lang: '+msg.from.language_code+', command: '+msg.text+', type: command received');
-				await bot.sendChatAction(chatid, 'typing')
-				temp = await modules.getlang(msg, logger);
-				try {
-					await bot.sendMessage(chatid, "🖼 "+temp.text(msg.chat.type, 'command.img.blank'), {reply_to_message_id: msg.message_id, reply_markup: {
-						force_reply: true, selective: true
-					}});
-					logger.info('chatid: '+chatid+', username: '+modules.getuser(msg.from)+', lang: '+msg.from.language_code+', command: '+msg.text+', type: valid');
-				} catch(e) {
-					logger.error('chatid: '+chatid+', username: '+modules.getuser(msg.from)+', lang: '+msg.from.language_code+', command: '+msg.text+', type: valid');
-					logger.debug(e.stack);
-				}
-			}
-			catch(e) {
-				logger.error('chatid: '+chatid+', username: '+modules.getuser(msg.from)+', lang: '+msg.from.language_code+', command: '+msg.text+', type: error send error');
-				logger.debug(e.stack);
-			}
-		}
-    });
+module.exports = {
+    
 }
---
+++ src/modules/c_lang.js
@@ -0,0 +1,33 @@
+module.exports = (bot, logger, helper) => {
+    bot.onText(new RegExp('^/\(?:언어변경|언어|lang|langset)+(?:@'+global.botinfo.username+')? ?$'), async(msg, match) => {
+		if(Math.round((new Date()).getTime() / 1000) - msg.date <= 180) {
+			const chatid = msg.chat.id;
+			let temp;
+			try {
+				logger.info('chatid: '+chatid+', username: '+helper.getuser(msg.from)+', lang: '+msg.from.language_code+', command: '+msg.text+', type: command received');
+				let send;
+                [send, temp] = await Promise.all([
+                    bot.sendChatAction(chatid, 'typing'),
+                    helper.getlang(msg, logger)
+                ]);
+				let ctype = msg.chat.type;
+				if(ctype == 'group' || ctype == 'supergroup' || ctype == 'channel') {
+					await bot.sendMessage(chatid, "❗️ "+temp.group('command.lang.isgroup'), {reply_to_message_id: msg.message_id, parse_mode: 'HTML', reply_markup: {
+							inline_keyboard: [[{
+								text: temp.inline('tobot'),
+								url: 'https://t.me/'+global.botinfo.username
+							}]]
+						}});
+				} else {
+					await bot.sendMessage(chatid, "🔤 "+temp.group('command.lang.announce'), {reply_to_message_id: msg.message_id, parse_mode: 'HTML', reply_markup: {
+						inline_keyboard: helper.langlist(temp)
+					}});
+				}
+				logger.info('chatid: '+chatid+', username: '+helper.getuser(msg.from)+', lang: '+msg.from.language_code+', command: '+msg.text+', type: valid,');
+			} catch(e) {
+				logger.error('chatid: '+chatid+', username: '+helper.getuser(msg.from)+', lang: '+msg.from.language_code+', command: '+msg.text+', type: error');
+				logger.debug(e.stack);
+			}
+		}
+	});
+}
