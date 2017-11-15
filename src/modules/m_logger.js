5b25a81bf52c65b14495aca5e781ec00f0c7c2cb
small_sunshine <jioo0224@naver.com>
2017-11-16 00:09:48 +0900

    로거 개선

--- src/modules/m_logger.js
+++ src/modules/m_logger.js
@@ -1,7 +1,7 @@
 module.exports = (bot, logger, helper) => {
     bot.on('message', (msg) => {
         if(typeof msg.text != 'undefined') {
-            logger.debug('chatid: '+msg.chat.id+', text: '+msg.text.replace(/\n/g, '\\n')+(typeof msg.chat.username != 'undefined' ? ', username: '+msg.chat.username : ', username: none'));
+            logger.debug('chatid: '+msg.chat.id+', text: '+msg.text.replace(/\n/g, '\\n')+', username: '+helper.getuser(msg.from));
         }
     });
 }
