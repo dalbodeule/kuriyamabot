55784d3d50b4708b942572f35db71ae0cada18d1
small_sunshine <jioo0224@naver.com>
2017-11-26 21:48:38 +0900

    inline help 명령어 및 언어변경 관련 개선

---
+++ src/modules/i_help.js
@@ -0,0 +1,36 @@
+module.exports = (bot, logger, helper) => {
+    bot.on('inline_query', async (msg) => {
+        const q = {
+            id: msg.id, query: msg.query
+        }
+        
+        const match = q.query.match(/^(?:([help]+)(?:| (.*)+))$/);
+        if(match) {
+            let temp;
+            try {
+                temp = await helper.getlang(msg, logger);
+                await bot.answerInlineQuery(q.id, [{type: 'article', title: 'help message', id: 'help', input_message_content: {
+                    message_text: temp.inline('command.help.help.name')+"\n\n"+
+                    "🖼 "+temp.inline('command.help.img.name')+"\n\n"+
+                    "🔍 "+temp.inline('command.help.search.name')+"\n\n"+
+                    "⚙️ "+temp.inline('tobot')
+                    , parse_mode: 'HTML'
+                }, reply_markup: {
+                    inline_keyboard: [[{
+                        text: "🖼",
+                        switch_inline_query_current_chat: 'img'
+                    }, {
+                        text: "🔍",
+                        switch_inline_query_current_chat: 'search'
+                    }], [{
+                            text: "⚙️",
+                            url: "https://t.me/"+global.botinfo.username
+                        }]]
+                }}], {cache_time: 3});
+            } catch(e) {
+                logger.error('inlineid: '+q.id+', username: '+helper.getuser(msg.from)+', lang: '+msg.from.language_code+', command: '+msg.query+', type: error');
+                logger.debug(e.stack);
+            }
+        }
+    });
+}
