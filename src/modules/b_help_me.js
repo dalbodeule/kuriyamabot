2ec175b9a426e16b4beeea1c4357113334d9e19b
small_sunshine <jioo0224@naver.com>
2017-12-13 17:47:46 +0900

    eslint 적용중..

--- src/modules/b_help_me.js
+++ src/modules/b_help_me.js
@@ -1,32 +1,37 @@
 module.exports = (bot, logger, helper) => {
-    const answer = (msg, temp) => {
-        bot.answerCallbackQuery(msg.id, temp.group('command.help.twice'));
-    }
-    
-    bot.on('callback_query', async (msg) => {
-        const callid = msg.id;
-        let temp;
-        try {
-            temp = await helper.getlang(msg, logger);
-            if(msg.data == 'help_me') {
-                if(msg.message.text != "📟 "+temp.help('command.help.me')) {
-                    try {
-                        await bot.editMessageText("📟 "+temp.help('command.help.me'), {chat_id: msg.message.chat.id, message_id: msg.message.message_id, 
-                            reply_to_message_id: msg.message_id, parse_mode: 'HTML', reply_markup: {
-                            inline_keyboard: helper.commandlist(temp)}});
-                        logger.info('callback id: '+callid+', username: '+helper.getuser(msg.from)+', lang: '+msg.from.language_code+', command: '+msg.data+', type: valid');     
-                    } catch(e) {
-                        logger.error('callback id: '+callid+', username: '+helper.getuser(msg.from)+', lang: '+msg.from.language_code+', command: '+msg.data+', type: error');
-                        logger.debug(e.stack);
-                    }
-                } else {
-                    answer(msg, temp);
-                }
-            }
-            logger.info('callback id: '+callid+', username: '+helper.getuser(msg.from)+', lang: '+msg.from.language_code+', command: '+msg.data+', type: callback received');
-        } catch(e) {
-            logger.error('callback id: '+callid+', username: '+helper.getuser(msg.from)+', lang: '+msg.from.language_code+', command: '+msg.text+', type: error');
-            logger.debug(e.message);
+  const answer = (msg, temp) => {
+    bot.answerCallbackQuery(msg.id, temp.group('command.help.twice'))
+  }
+
+  bot.on('callback_query', async (msg) => {
+    const callid = msg.id
+    let temp
+    try {
+      temp = await helper.getlang(msg, logger)
+      if (msg.data === 'help_me') {
+        if (msg.message.text !== '📟 ' + temp.help('command.help.me')) {
+          try {
+            await bot.editMessageText('📟 ' + temp.help('command.help.me'), {
+              chat_id: msg.message.chat.id,
+              message_id: msg.message.message_id,
+              reply_to_message_id: msg.message_id,
+              parse_mode: 'HTML',
+              reply_markup: {
+                inline_keyboard: helper.commandlist(temp)
+              }})
+            logger.info('callback id: ' + callid + ', username: ' + helper.getuser(msg.from) + ', lang: ' + msg.from.language_code + ', command: ' + msg.data + ', type: valid')
+          } catch (e) {
+            logger.error('callback id: ' + callid + ', username: ' + helper.getuser(msg.from) + ', lang: ' + msg.from.language_code + ', command: ' + msg.data + ', type: error')
+            logger.debug(e.stack)
+          }
+        } else {
+          answer(msg, temp)
         }
-    });
+      }
+      logger.info('callback id: ' + callid + ', username: ' + helper.getuser(msg.from) + ', lang: ' + msg.from.language_code + ', command: ' + msg.data + ', type: callback received')
+    } catch (e) {
+      logger.error('callback id: ' + callid + ', username: ' + helper.getuser(msg.from) + ', lang: ' + msg.from.language_code + ', command: ' + msg.text + ', type: error')
+      logger.debug(e.message)
+    }
+  })
 }
