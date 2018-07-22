cff40d4118f18c1cab2f29a4b1f80f6e2d59177c
small_sunshine <jioo0224@naver.com>
2018-07-22 16:35:08 +0900

    오타 수정한 1.0.0.-beta-3

--- src/lang/lang_en.js
+++ src/lang/lang_en.js
@@ -1,4 +1,4 @@
-module.export = {
+module.exports = {
   lang: {
     langname: 'English',
     display: '🇺🇸 English',
--- src/lang/lang_ko.js
+++ src/lang/lang_ko.js
@@ -1,4 +1,4 @@
-module.export = {
+module.exports = {
   lang: {
     langname: 'Korean',
     display: '🇰🇷 한국어',
