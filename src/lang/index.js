7e4c12c9218e96965cec46202a3571437dd64eb9
small_sunshine <jioo0224@naver.com>
2018-08-12 19:57:50 +0900

    에러 수정

--- src/lang/index.js
+++ src/lang/index.js
@@ -43,7 +43,7 @@ module.exports = class {
         logger.debug('id: ' + this.id + ', lang: ' + this.lang)
         return query
       }
-    } else if (msg.message.chat) { // callback query 대응
+    } else if (msg.message) { // callback query 대응
       this.id = (msg.message.chat.type === 'private' ? msg.from.id : msg.message.chat.id)
       this.logger = logger
       let query = await model.language.find(this.id)
