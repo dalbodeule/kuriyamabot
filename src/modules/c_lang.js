1d73a0edf52bb97317223ced49b91fffb885b60e
small_sunshine <jioo0224@naver.com>
2018-08-12 18:55:51 +0900

    #4 에러 수정

--- src/modules/c_lang.js
+++ src/modules/c_lang.js
@@ -2,18 +2,18 @@ module.exports = (bot, logger, helper) => {
   bot.onText(new RegExp('^/(?:언어변경|언어|lang|langset)+(?:@' + global.botinfo.username + ')? ?$'), async (msg, match) => {
     if (Math.round((new Date()).getTime() / 1000) - msg.date <= 180) {
       const chatid = msg.chat.id
-      let temp
+      let temp, send
       try {
         logger.info('chatid: ' + chatid + ', username: ' + helper.getuser(msg.from) + ', lang: ' + msg.from.language_code + ', command: ' + msg.text + ', type: command received')
-        // eslint-disable-next-line
-        let send, admins, isAdmin = false;
-        [send, temp, admins] = await Promise.all([
-          bot.sendChatAction(chatid, 'typing'),
-          helper.getlang(msg, logger),
-          bot.getChatAdministrators(chatid)
-        ])
         let ctype = msg.chat.type
         if (ctype === 'group' || ctype === 'supergroup' || ctype === 'channel') {
+          // eslint-disable-next-line
+          let admins, isAdmin = false;
+          [send, temp, admins] = await Promise.all([
+            bot.sendChatAction(chatid, 'typing'),
+            helper.getlang(msg, logger),
+            bot.getChatAdministrators(chatid)
+          ])
           isAdmin = isAdmin = admins.some((v) => {
             return v.user.id === msg.from.id
           })
@@ -30,6 +30,10 @@ module.exports = (bot, logger, helper) => {
             })
           }
         } else {
+          [send, temp] = await Promise.all([
+            bot.sendChatAction(chatid, 'typing'),
+            helper.getlang(msg, logger)
+          ])
           await bot.sendMessage(chatid, '🔤 ' + temp.text('command.lang.announce'), {
             reply_to_message_id: msg.message_id,
             parse_mode: 'HTML',
