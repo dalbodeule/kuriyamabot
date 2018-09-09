5d1f793d18fa36af75c47b093465379ea8726c92
small_sunshine <jioo0224@naver.com>
2018-09-09 21:52:28 +0900

    모듈 이름 수정 + /msginfo 추가

---
+++ src/modules/command_msginfo.js
@@ -0,0 +1,28 @@
+module.exports = (bot, logger, helper) => {
+  bot.onText(new RegExp('^/msginfo+(?:@' + global.botinfo.username + ')? ?$'), async (msg, match) => {
+    if (Math.round((new Date()).getTime() / 1000) - msg.date <= 180) {
+      const chatid = msg.chat.id
+      let temp
+      try {
+        logger.info('chatid: ' + chatid + ', username: ' + helper.getuser(msg.from) + ', lang: ' + msg.from.language_code + ', command: ' + msg.text + ', type: command received')
+        // eslint-disable-next-line
+        let send;
+        [send, temp] = await Promise.all([
+          bot.sendChatAction(chatid, 'typing'),
+          helper.getlang(msg, logger)
+        ])
+        if (msg.reply_to_message) {
+          await bot.sendMessage(chatid, temp.text('command.msginfo.success') + '\n\n`' + msg.reply_to_message + '`', { reply_to_message_id: msg.message_id,
+            parse_mode: 'Markdown'})
+        } else {
+          await bot.sendMessage(chatid, temp.text('command.msginfo.alert'), { reply_to_message_id: msg.message_id,
+            parse_mode: 'HTML'})
+        }
+        logger.info('chatid: ' + chatid + ', username: ' + helper.getuser(msg.from) + ', lang: ' + msg.from.language_code + ', command: ' + msg.text + ', type: valid')
+      } catch (e) {
+        logger.error('chatid: ' + chatid + ', username: ' + helper.getuser(msg.from) + ', lang: ' + msg.from.language_code + ', command: ' + msg.text + ', type: error')
+        logger.debug(e.stack)
+      }
+    }
+  })
+}
