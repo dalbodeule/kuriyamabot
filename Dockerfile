aee9be68905f08e715975bb9daf8ef2ddd45455f
small_sunshine <jioo0224@naver.com>
2018-09-17 23:25:53 +0900

    typescript 빌드 관련 수정

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ FROM node:latest
 COPY . /src
 WORKDIR /src
 RUN npm install
+RUN npm run build
 
 CMD ["node", "./dist/index.js"]
--- dist/db/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const language_1 = require("./model/language");
-exports.language = language_1.default;
-const message_1 = require("./model/message");
-exports.message = message_1.default;
