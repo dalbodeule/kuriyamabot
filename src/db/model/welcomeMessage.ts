bae0427e7b0922abb6caf031242eeb4c259b24c3
small_sunshine <jioo0224@naver.com>
2018-12-22 16:42:59 +0900

    DB Query 관련 수정

--- src/db/model/welcomeMessage.ts
+++ src/db/model/welcomeMessage.ts
@@ -32,6 +32,12 @@ class Message {
   }
 
   static async create (id: number, message: string): Promise<boolean> {
+    await db.User.findOrCreate({
+      where: {
+        id
+      }
+    })
+
     await db.WelcomeMessage.create({
       userId: id,
       message
